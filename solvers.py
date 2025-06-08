8ba8267e30c6c692030a9f78de96a2747e7f57bb
Jonathan Gill <therealjtgill@gmail.com>
2025-06-08 15:37:22 -0400

    Finished a basic implementation of infeasible start newton's method.

--- solvers.py
+++ solvers.py
@@ -13,7 +13,49 @@ class InfeasibleStartNewton:
          ]
       )
 
-   def solve(self, x_init: np.ndarray, v_init: np.ndarray, max_num_iters: int):
+   def solve(
+      self,
+      x_init: np.ndarray,
+      v_init: np.ndarray,
+      alpha: float,
+      beta: float,
+      max_num_iters: int,
+      eps: float=1e-8
+   ):
       assert(len(v_init.shape) == 1)
       assert(v_init.shape[0] == self.qp.M)
-      
+      assert(alpha > 0)
+      assert(beta > 0)
+      assert(max_num_iters > 0)
+
+      x = np.copy(x_init)
+      v = np.copy(v_init)
+
+      t = 20.0
+      residual = self.residual(x, v, t)
+
+      num_iters = 0
+
+      while np.linalg.norm(residual) >= eps and (num_iters < max_num_iters):
+         # hess = self.qp.hessian(x, t)
+
+         residual = self.residual(x, v, t)
+         K = self.qp.kkt_matrix(x, t)
+
+         delta_x_v = np.linalg.solve(K, -residual)
+         delta_x = delta_x_v[0:self.qp.N]
+         delta_v = delta_x_v[self.qp.N:]
+
+         s = 1.0
+         while (
+            not self.qp.in_domain(x + s * delta_x)
+            or np.linalg.norm(self.residual(x + s * delta_x, v + s * delta_v, t)) > (1.0 - alpha * s) * np.linalg.norm(residual)
+         ):
+            s *= beta
+
+         x += s * delta_x
+         v += s * delta_v
+
+         num_iters += 1
+
+      return x, v, np.linalg.norm(self.residual(x, v, t))
