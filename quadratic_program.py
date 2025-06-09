Fixed incorrect assertions in 'QuadraticProgram' for vector and matrix sizes. Fixed incorrect gradient calculation (there's got to be a faster way to do the log-barrier gradient calculation using numpy built-in functions).

quadratic_program.py
@@ -14,7 +14,7 @@ class QuadraticProgram:
       self.P = C.shape[0]
 
       assert(np.linalg.matrix_rank(A) == self.M)
-      assert(np.linalg.matrix_rank(C) == self.P)
+      # assert(np.linalg.matrix_rank(C) == self.P)
 
       assert(self.M < self.N)
       assert(self.P < self.N)
@@ -24,15 +24,17 @@ class QuadraticProgram:
       assert(len(d.shape) == 1)
 
       assert(p.shape[0] == self.N)
-      assert(b.shape[0] == self.N)
-      assert(d.shape[0] == self.N)
+      assert(b.shape[0] == self.M)
+      assert(d.shape[0] == self.P)
 
       assert(self.N > 0)
       assert(self.N == Q.shape[1])
 
+      print("C shape: ", C.shape)
+
       self._partial_kkt_mat = np.zeros((self.N + self.M, self.N + self.M))
       self._partial_kkt_mat[self.N: self.N + self.M, 0: self.N] = self.A
-      self._partial_kkt_mat[0: self.N, self.N: self.N + self.M] = self.A.transpose
+      self._partial_kkt_mat[0: self.N, self.N: (self.N + self.M)] = self.A.transpose()
 
    def objective(self, x: np.ndarray, t: float) -> float:
       assert(len(x.shape) == 1)
@@ -46,7 +48,15 @@ class QuadraticProgram:
       assert(x.shape[0] == self.N)
       assert(t > 0)
 
-      return np.dot(self.Q, x) - (1.0 / t) * (self.C / (np.dot(self.C, x) - self.d))
+      # return np.dot(self.Q, x) - (1.0 / t) * (self.C / (np.dot(self.C, x) - self.d))
+      loss_grad = np.dot(self.Q, x)
+
+      barrier_grad = np.zeros_like(loss_grad)
+      for i in range(self.C.shape[0]):
+         barrier_grad += self.C[i, :] / (np.dot(self.C[i, :], x) - self.d[i])
+      barrier_grad *= (-1.0 / t)
+
+      return loss_grad + barrier_grad
 
    def hessian(self, x: np.ndarray, t: float):
       assert(len(x.shape) == 1)
@@ -54,7 +64,7 @@ class QuadraticProgram:
       assert(t > 0)
 
       hess = np.zeros_like(self.Q)
-      linear_terms = self.dot(self.C, x) - self.d
+      linear_terms = np.dot(self.C, x) - self.d
       for i in range(self.P):
          hess += np.outer(self.C[i], self.C[i]) / linear_terms[i]
       hess *= (1.0 / t)