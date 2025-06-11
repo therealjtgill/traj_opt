1e2b74683a96dc298e0ef6747f9f3161048df42f
Jonathan Gill <therealjtgill@gmail.com>
2025-06-10 21:53:36 -0400

    #9 - Feasible start newton class (#11)
    * Added a rough initial cut of feasible start Newton's method.
    
    * Fixed bug in backtracking line search where 'x' was used instead of 'x + s * delta_x'. Made 'solve' methods take 't' value for barrier penalties as an argument.
    
    * Added feasible start Newton's method to the new LMPC solver function ('do_stuff').
    
    * Added arguments for plotting and saving plots to 'cleaner_trajectory_optimization'.
    
    * Replaced 'cartpole_trajectory_optimizer' with the new function.
    
    * Deleted commented out code.
    
    * Fixed call to non-existent variable in IFSNM.
    
    * Started incorporating new classes into the more complicated optimization function.
    
    * Made the order of parameters to the 'solve' methods make more sense (log barrier coefficient no longer has a default value).
    
    * Added a separate 'QuadraticProgram.initialize' method. Got rid of the partial KKT matrix calculation at initialization. Fixed a bug where the Hessian of the log barrier terms was calculated incorrectly (denominator needed to be squared).
    
    * Replaced the ISNM function calls in the relinearizing trajectory optimizer with the ISNM class.
    
    * Added more output telemetry to the feasible start newton solver.
    
    * The feasible start newton solver works! It's substituted into the relinearizing trajectory optimization function.
    
    * Deleted old optimization functions from the jupyter notebook.
    
    * Now with output!

--- solvers.py
+++ solvers.py
@@ -1,10 +1,11 @@
+import copy
 import numpy as np
 from quadratic_program import QuadraticProgram
 
 class InfeasibleStartNewton:
    '''
    Implementation of Infeasible Start Newton's Method for a convex optimization
-   problem with linear equality constraints.
+   problem with linear equality constraints. 
    '''
    def __init__(self, qp: QuadraticProgram):
       self.qp = qp
@@ -29,14 +30,16 @@ class InfeasibleStartNewton:
       self,
       x_init: np.ndarray,
       v_init: np.ndarray,
-      alpha: float,
-      beta: float,
-      max_num_iters: int,
+      t: float,
+      alpha: float = 0.5,
+      beta: float = 0.9,
+      max_num_iters: int = 100,
       eps: float=1e-8
    ):
       '''
       x_init - Initial value for the decision variable, `x`. The initial guess
-         must be in the domain of the problem definition
+         must be in the domain of the problem definition, but it does not have
+         to be feasible.
       v_init - Initial value for the equality constraint Lagrange multiplier, `v`
       alpha - Parameter for line search over residual values
       beta - Parameter to reduce line search magnitude by
@@ -51,32 +54,97 @@ class InfeasibleStartNewton:
       assert(beta > 0)
       assert(max_num_iters > 0)
 
-      x = np.copy(x_init)
-      v = np.copy(v_init)
+      x = copy.deepcopy(x_init)
+      v = copy.deepcopy(v_init)
 
-      t = 20.0
-      residual = self.residual(x, v, t)
-
+      res = self.residual(x, v, t)
       num_iters = 0
 
-      while np.linalg.norm(residual) >= eps and (num_iters < max_num_iters):
-         residual = self.residual(x, v, t)
+      while np.linalg.norm(res) >= eps and (num_iters < max_num_iters):
+         res = self.residual(x, v, t)
          K = self.qp.kkt_matrix(x, t)
 
-         delta_x_v = np.linalg.solve(K, -residual)
+         delta_x_v = np.linalg.solve(K, -res)
          delta_x = delta_x_v[0:self.qp.N]
          delta_v = delta_x_v[self.qp.N:]
 
          s = 1.0
          while (
             not self.qp.in_domain(x + s * delta_x)
-            or np.linalg.norm(self.residual(x + s * delta_x, v + s * delta_v, t)) > (1.0 - alpha * s) * np.linalg.norm(residual)
+            or np.linalg.norm(self.residual(x + s * delta_x, v + s * delta_v, t)) > (1.0 - alpha * s) * np.linalg.norm(res)
          ):
             s *= beta
 
          x += s * delta_x
          v += s * delta_v
+         print("ifsnm num iters:", num_iters, "residual norm:", np.linalg.norm(res), "line search param:", s)
 
          num_iters += 1
 
       return x, v, np.linalg.norm(self.residual(x, v, t))
+
+class FeasibleStartNewton:
+   def __init__(self, qp: QuadraticProgram):
+      self.qp = qp
+
+   def newton_decrement(self, x: np.ndarray, t: float) -> float:
+      pass
+
+   def solve(
+      self,
+      x_init: np.ndarray,
+      v_init: np.ndarray,
+      t: float,
+      alpha: float = 0.5,
+      beta: float = 0.9,
+      max_num_iters: int = 100,
+      eps: float=1e-8
+   ):
+      assert(len(v_init.shape) == 1)
+      assert(v_init.shape[0] == self.qp.M)
+      assert(alpha > 0)
+      assert(beta > 0)
+      assert(max_num_iters > 0)
+
+      x = copy.deepcopy(x_init)
+      v = copy.deepcopy(v_init)
+
+      num_iters = 0
+
+      grad = self.qp.gradient(x, t)
+      hess = self.qp.hessian(x, t)
+      K = self.qp.kkt_matrix(x, t)
+      delta_x_v = np.linalg.solve(K, np.hstack([-grad, np.zeros((self.qp.M,))]))
+      delta_x = delta_x_v[0:self.qp.N]
+      v = delta_x_v[self.qp.N:]
+
+      newton_dec = np.sqrt(np.dot(np.dot(delta_x, hess), delta_x))
+
+      print("entering fsnm loop")
+      print("newton decrement:", newton_dec)
+
+      while newton_dec >= (2.0 * eps) and (num_iters < max_num_iters):
+         obj = self.qp.objective(x, t)
+         grad = self.qp.gradient(x, t)
+         hess = self.qp.hessian(x, t)
+         K = self.qp.kkt_matrix(x, t)
+         delta_x_v = np.linalg.solve(K, np.hstack([-grad, np.zeros((self.qp.M,))]))
+         delta_x = delta_x_v[0:self.qp.N]
+         v = delta_x_v[self.qp.N:]
+
+         newton_dec = np.sqrt(np.dot(np.dot(delta_x, hess), delta_x))
+
+         s = 1.0
+         while (
+            not self.qp.in_domain(x + s * delta_x)
+            or self.qp.objective(x + s * delta_x, t) > obj + alpha * s * np.dot(grad, delta_x)
+         ):
+            s *= beta
+
+         x = x + s * delta_x
+
+         print("fsnm num iters:", num_iters, "line search:", s, "newton decrement:", newton_dec)
+
+         num_iters += 1
+
+      return x, v, (num_iters > 0)
