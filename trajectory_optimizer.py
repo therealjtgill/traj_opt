e3b84570b9ec0ca93c788016939b1ade3633164c
Jonathan Gill <therealjtgill@gmail.com>
2025-06-12 16:20:50 -0400

    Sea of Thieves time.

--- trajectory_optimizer.py
+++ trajectory_optimizer.py
@@ -1,7 +1,9 @@
+import copy
 import numpy as np
 from typing import List
 
 from cartpole_dynamics import CartpoleDynamics
+from quadratic_program import QuadraticProgram
 from solvers import FeasibleStartNewton, InfeasibleStartNewton
 
 class TrajectoryOptimizer:
@@ -15,4 +17,12 @@ class TrajectoryOptimizer:
       reference_trajectory: List[np.ndarray] = None,
       relinearization_sequence: List[np.ndarray] = None
    ):
-      pass
+      self.dynamics = dynamics
+      self.num_collocation_points = num_collocation_points
+      self.time_horizon = time_horizon
+      self.initial_state = initial_state
+      self.final_state = final_state
+      self.reference_trajectory = reference_trajectory
+      self.relinearization_sequence = relinearization_sequence
+
+      self.qp = QuadraticProgram()
